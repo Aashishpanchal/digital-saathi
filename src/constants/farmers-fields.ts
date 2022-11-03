035797b76ce6c9e2874d8ccdcc43f4e8f13e3a9a
Aashish Panchal <aipanchal51@gmail.com>
2022-11-03 22:28:09 +0530

    new code

---
+++ src/constants/farmers-fields.ts
@@ -0,0 +1,24 @@
+export const farmersFields = [
+  {
+    label: "S.No",
+    key: "s_no",
+  },
+  {
+    label: "auth_code",
+    key: "auth_code",
+  },
+  {
+    label: "farmer name",
+    key: "customer_name",
+  },
+  {
+    label: "farmer name",
+    key: "email_id",
+  },
+  {
+    label: "phone_no",
+    key: "phone_no",
+  },
+];
+
+export const farmersOrdersFields = [{}];
