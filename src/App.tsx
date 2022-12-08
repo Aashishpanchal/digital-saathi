a4d303cfa0f30406554f4dc2d61e42ee484a7ed7
KAPIL BAMBA (CRGL-THIRDPARTY.COM) <kapil_bamba@crgl-thirdparty.com>
2022-12-08 13:47:47 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,26 @@ import LoadingDialogBox from "./components/dialog-box/loading-dialog-box";
 import { SnackbarProvider } from "notistack";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import {
+  useLocation
+} from "react-router-dom"
 import React from "react"
 import axios from "axios"
 import qs from "qs"
 
+
+
 export default function App() {
+  const { search } = useLocation()
+  const [refreshToken, setRefreshToken] = React.useState("")
+
+
+
+
+  React.useEffect(() => {
+    const refreshToken = search.split("=")[1]
+    setRefreshToken(refreshToken)
+  }, [])
 
 
   const {
@@ -20,25 +35,31 @@ export default function App() {
   } = useSelector((state: RootState) => state);
 
   React.useEffect(() => {
-    axios.post("https://cargillcustomer-qa.oktapreview.com/oauth2/aushb5mlqe4IiZu3k0h7/v1/token", qs.stringify({
-      grant_type: "password",
-      redirect_uri: "https://digitalsaathi-cms.dev.cglcloud.in/login",
-      username: "support@digitalsaathi.com",
-      password: "tkq6exz.txv7qpu_PFN",
-      scope: "openid profile email offline_access",
-      client_id: "0oauvcg8f8ccy7CBU0h7"
-    }), {
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": "JSESSIONID=82716910C0B3A83A8C773C434FAE070F"
+    if (refreshToken) {
+
+      const data1 = {
+        grant_type: "refresh_token",
+        redirect_uri: "https://digitalsaathi-cms.cglcloud.com/login",
+        scope: "offline_access openid profile",
+        refresh_token: refreshToken,
+        client_id: "0oauvcg8f8ccy7CBU0h7"
       }
-    }).then(response => {
-      console.log(response.data)
-    })
+      axios.post("https://cargillcustomer-qa.oktapreview.com/oauth2/aushb5mlqe4IiZu3k0h7/v1/token", qs.stringify(data1), {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Cookie": "JSESSIONID=FE2FEF078D9A3009EC18B08AAC797497"
+        }
+      }).then(response => {
+        if (response.data) {
+          response.data.access_token && localStorage.setItem("access_token", response.data.access_token)
+          response.data.refresh_token && localStorage.setItem("refresh_token", response.data.refresh_token)
+        }
+      }).catch((err) => {
+        console.log(err.response.data)
+      })
 
-
-  }, [])
+    }
+  }, [refreshToken])
 
 
 
